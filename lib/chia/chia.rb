a3ef60d136fd04c671e17cb44777891cc37de1f1
Camilo Ribeiro <camilo.ribeiro@magine.com>
2016-04-11 20:29:28 +0200

    Adding support to basics of a service

--- lib/chia/chia.rb
+++ lib/chia/chia.rb
@@ -1,7 +1,24 @@
 require 'sinatra/base'
+require 'yaml'
+require 'json'
+require 'datagrid'
 
 class Chia < Sinatra::Base
   get '/' do
-    "Hello World!"
+    get_services
+  end
+
+  get '/json' do
+    JSON.pretty_generate get_services
+  end
+
+  get "/json/:service" do
+    #require "pry"; binding.pry
+    JSON.pretty_generate get_services["services"][params['service']]
+  end
+
+  private
+  def get_services
+    YAML.load_file('../services.yml')
   end
 end
--- spec/chia_spec.rb
+++ spec/chia_spec.rb
@@ -8,10 +8,30 @@ describe Chia do
   end
 
   describe "Get Operations" do
-    it "GET: get to /" do
-      get '/'
+
+    it "Get all services as a json" do
+      get '/json'
       expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq("Hello World!")
+      expect(last_response.body).to eq(
+        '{
+  "services": {
+    "service-in-scala": {
+      "health-url": "http://scala.com"
+    },
+    "service-in-java": {
+      "health-url": "http://java.com"
+    }
+  }
+}')
+    end
+    
+    it "Get data from specific service" do
+      get '/json/service-in-scala'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq(
+        '{
+  "health-url": "http://scala.com"
+}')
     end
   end
 end
