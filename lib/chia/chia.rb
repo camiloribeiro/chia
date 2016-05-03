3a31b7467ae171e779614d6577abd11f67bccd28
Camilo Ribeiro <camilo@camiloribeiro.com>
2016-05-03 18:18:08 +0200

    Working :D

--- lib/chia/chia.rb
+++ lib/chia/chia.rb
@@ -30,16 +30,17 @@ class Chia < Sinatra::Base
   end
 
   get "/status/:service" do
-    (get_status_for_service params['service']).to_json
+    #/(get_status_for_service params['service']).to_json
   end
 
   get "/status" do
+#'[{"name": "service-in-scala", "health": "health"},{"name": "service-in-java", "health": "health"}]'
     services = JSON.parse get_services["services"].to_json
-    response = Array.new
+    response = []
     services.each do |service_name, service_info|
-      response << {:name => service_name, :health => (get_status_for_service service_name)}
+      response << "{\"name\": \"#{service_name}\", \"health\": \"#{(get_status_for_service service_name)}\"}"
     end
-    response.to_json
+    "[#{response.join(",")}]"
   end
 
   private
@@ -54,9 +55,8 @@ class Chia < Sinatra::Base
     rescue => e
       response = e.response
     end
-    expectation = get_services["services"][service_name]["status"]["expected-response"]
 
-    
+    expectation = get_services["services"][service_name]["status"]["expected-response"]
     (response == expectation)
   end
 end
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,25 +4,63 @@ var data = [
 ];
 
 var ServiceBox = React.createClass({
-  render: function() {
-            return (
-              <div className="serviceBox">
-              <ServiceList data={this.props.data} />
-              </div>
-              );
-          }
+  loadServiceStatusFromServer: function() {
+                                 $.ajax({
+                                   url: this.props.url,
+                                   dataType: 'json',
+                                   cache: false,
+                                   success: function(data) {
+                                     this.setState({data: data});
+                                   }.bind(this),
+                                   error: function(xhr, status, err) {
+                                            console.error(this.props.url, status, err.toString());
+                                          }.bind(this)
+                                 });
+                               },
+    getInitialState: function() {
+                       return {data: []};
+                     },
+    componentDidMount: function() {
+                         this.loadServiceStatusFromServer();
+                         setInterval(this.loadServiceStatusFromServer, this.props.pollInterval);
+                       },
+    render: function() {
+              return (
+                  <div className="serviceBox">
+                  <ServiceList data={this.state.data} />
+                  </div>
+                  );
+            }
+});
+
+var Service = React.createClass({
+  rawMarkup: function() {
+               var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+               return { __html: rawMarkup };
+             },
+
+    render: function() {
+              return (
+                <div className="service">
+                <h2 className="servicehealth">
+                {this.props.health}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+                </div>
+                );
+            }
 });
 
 var ServiceList = React.createClass({
   render: function() {
             var serviceNodes = this.props.data.map(function(service) {
-                    return (
-                              <Service health={service.author} key={service.name}>
-                                        {service.name}
-                                        {service.health}
-                                                </Service>
-                                                      );
-                        });
+              return (
+                <Service health={service.health} key={service.name}>
+                {service.name}
+                {service.health}
+                </Service>
+                );
+            });
             return (
               <div className="serviceList">
               {serviceNodes}
@@ -31,25 +69,8 @@ var ServiceList = React.createClass({
           }
 });
 
-var Service = React.createClass({
-   rawMarkup: function() {
-                    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-                        return { __html: rawMarkup };
-                          },
-
-  render: function() {
-            return (
-              <div className="service">
-              <h2 className="servicehealth">
-              {this.props.health}
-              </h2>
-               <span dangerouslySetInnerHTML={this.rawMarkup()} />
-              </div>
-              );
-          }
-});
 
 ReactDOM.render(
-     <ServiceBox url="/status" />,
+    <ServiceBox url="/status" pollInterval={2000} />,
     document.getElementById('content')
     );
